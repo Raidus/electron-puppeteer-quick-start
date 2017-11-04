7ef97a3c4c53df43949f03b4ab00f694f64df49f
Willy Raider <willy.raider@gmail.com>
2017-11-04 11:45:08 +0100

    initial upload

---
+++ renderer.js
@@ -0,0 +1,25 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const app = require('electron').remote.app,
+  path = require('path'),
+  fs = require('fs'),
+  dialog = require('electron').remote.dialog,
+  BrowserWindow = require('electron').remote.BrowserWindow,
+  puppeteer = require('puppeteer');
+
+async function getPic(url) {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.setViewport({ width: 1200, height: 800 });
+  await page.screenshot({ path: 'google.png' });
+
+  await browser.close();
+}
+
+document.getElementById('start').addEventListener('click', function() {
+  event.preventDefault();
+  console.log('start');
+  getPic('https://www.google.de');
+});
